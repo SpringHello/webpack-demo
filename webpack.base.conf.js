2d97c8e7a2f12fec88938e7491b576d7fbad4613
unknown <204589369@qq.com>
2020-05-09 18:52:54 +0800

    insert

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -2,7 +2,7 @@ const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, './src/entry/three.js'),
+  entry: path.resolve(__dirname, './src/entry/three1.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js'
